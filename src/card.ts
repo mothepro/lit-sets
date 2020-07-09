f820cc6a5f848002e9eb95b2a11d2377fa90ecbc
mothepro <4e688d8adf9b8f68774900f9809778ccb7ef857a>
2020-07-09 00:58:48 -0500

    Show selected cards better

--- src/card.ts
+++ src/card.ts
@@ -22,7 +22,7 @@ export default class extends LitElement {
   @property({ type: Number })
   index = 0
 
-  @property({ type: Boolean })
+  @property({ type: Boolean, reflect: true })
   selected = false
 
   static readonly styles = css`
@@ -33,15 +33,35 @@ export default class extends LitElement {
 
     :host {
       display: inline-block;
+      position: relative;
+      margin-bottom: 5px;
     }
 
     :host([index]) {
       animation: 1s ease zoom-in both;
     }
 
-    :host sets-shape {
-      font-size: 50px;
-      margin: .2em;
+    :host:before {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      z-index: 1;
+      background-color: var(--selected-color-bg, black);
+      content: "";
+      pointer-events: none;
+      opacity: 0;
+      border-radius: 5px;
+    }
+
+    :host([selected]):before {
+      opacity: 0.15;
+    }
+
+    sets-shape {
+      font-size: var(--sets-shape-size, 50px);
+      margin: var(--sets-shape-spacing, .2em);
     }`
 
   /** This needs to override the value set. */
@@ -53,21 +73,21 @@ export default class extends LitElement {
 
   protected async firstUpdated() {
     await this.updateComplete
-    const mwcRoot = this.shadowRoot?.getElementById('mwc-button')?.shadowRoot
+
+    // TODO since <mwc-card> isn't launched yet.
+    const mwcRoot = this.shadowRoot?.firstElementChild?.shadowRoot
     if (mwcRoot)
       injectStyle(mwcRoot, css`#button { height: auto }`)
-    addEventListener('click', console.warn)
   }
 
-  protected readonly render = () => html`${
-    // There has gotta be a better way to do this
-    this.index ? html`<style>${this.myStyle}</style>` : ''}
-    <mwc-button raised id="mwc-button" @click=${() => this.selected = !this.selected}>
+  protected readonly render = () => html`
+    <mwc-button raised fullwidth  @click=${() => this.selected = !this.selected}>
       ${[...Array(1 + this.quantity)].map(() => html`
         <sets-shape
           opacity=${this.opacity}
           color=${this.color}
           shape=${this.shape}
         ></sets-shape>`)}
-    </mwc-button>`
+    </mwc-button>
+    ${this.index ? html`<style>${this.myStyle}</style>` : '' /* There has gotta be a better way to do this */}`
 }
