be414f415a5469576afbe13a00b7535d938d84e2
mothepro <4e688d8adf9b8f68774900f9809778ccb7ef857a>
2020-07-11 13:52:48 -0500

    Change offline button to icon at the top

--- demo/index.ts
+++ demo/index.ts
@@ -64,7 +64,7 @@ export default class extends LitElement {
       height: 1em;
     }`,
     
-    // Add color slots for the chart
+    // CSS vars for the colors in the chart
     ...[...Array(10)].map((_, i) => css`
       ::part(for-${i}) {
         stroke: var(--chart-color-${i});
