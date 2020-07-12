b0ce19f788aef77848bb5f9b137418fd0c8f009e
mothepro <4e688d8adf9b8f68774900f9809778ccb7ef857a>
2020-07-11 21:42:46 -0500

    Static styles for shapes

--- src/shape.ts
+++ src/shape.ts
@@ -1,11 +1,26 @@
 import { LitElement, customElement, property, css, html } from 'lit-element'
-import { Details } from 'sets-game-engine'
 
-// Equilateral Triangle Ratio. Thanks Amima :)
-const baseByHeight = Math.sqrt(3) / 3
+/** Convert a Color and opacity into */
+function cssColor(color: Details.Color, opacity = Details.Opacity.SOLID) {
+  switch (color) {
+    case Details.Color.BLUE:
+      return css`hsla(207, 90%, 58%, ${1 - opacity / 2})`
+    case Details.Color.RED:
+      return css`hsla(45, 100%, 50%, ${1 - opacity / 2})`
+    case Details.Color.GREEN:
+      return css`hsla(4, 90%, 58%, ${1 - opacity / 2})`
+  }
+}
 
-/** Border width ratio */
-const borderSize = 1 / 6
+const
+  /** Equilateral Triangle Ratio. Thanks Amima :) */
+  baseByHeight = Math.sqrt(3) / 3,
+  /** Border width ratio */
+  borderSize = 1 / 6,
+  /** All possible colors */
+  colors = [Details.Color.RED, Details.Color.GREEN, Details.Color.BLUE],
+  /** All possible opacities */
+  opacities = [Details.Opacity.EMPTY, Details.Opacity.HALF, Details.Opacity.SOLID]
 
 const y = borderSize * baseByHeight,
   x = Math.sqrt(borderSize ** 2 - y ** 2),
@@ -27,7 +42,7 @@ export default class extends LitElement {
   @property({ type: Number })
   shape!: Details.Shape
 
-  static readonly styles = css`
+  static readonly styles = [css`
   :host {
     display: inline-block;
     border-style: solid;
@@ -37,7 +52,7 @@ export default class extends LitElement {
     border-width: ${borderSize}em;
   }
   :host([shape="${Details.Shape.SQUARE}"]) {
-    border-radius: 0.25em;
+    border-radius: 0.1em;
   }
   :host([shape="${Details.Shape.CIRCLE}"]) {
     border-radius: 50%;
@@ -60,39 +75,33 @@ export default class extends LitElement {
     border-style: solid;
     border-color: transparent;
     border-bottom-color: var(--mdc-theme-primary, white);
-  }`
+  }`,
 
-  protected get myStyle() {
-    if (this.shape == Details.Shape.TRIANGLE)
-      return css`
-        :host(sets-shape) {
-          border-bottom-color: ${this.cssColor()};
-        }
-        :host span {
-          opacity: ${1 - this.opacity / 2};
-        }`
-
-    return css`
-      :host(sets-shape) {
-        border-color: ${this.cssColor()};
-        background-color: ${this.cssColor(this.opacity / 2)};
-      }`
-  }
-
-  private cssColor(opacity = 1) {
-    switch (this.color) {
-      case Details.Color.BLUE:
-        return css`hsla(207, 90%, 58%, ${opacity})`
-      case Details.Color.RED:
-        return css`hsla(45, 100%, 50%, ${opacity})`
-      case Details.Color.GREEN:
-        return css`hsla(4, 90%, 58%, ${opacity})`
+  // Shape border
+  ...colors.map(color => css`
+    :host([shape="${Details.Shape.SQUARE}"][color="${color}"]),
+    :host([shape="${Details.Shape.CIRCLE}"][color="${color}"]) {
+      border-color: ${cssColor(color)};
     }
-  }
+    :host([shape="${Details.Shape.TRIANGLE}"][color="${color}"]) {
+      border-bottom-color: ${cssColor(color)};
+    }`),
 
-  protected readonly render = () => html`
-    <style>${this.myStyle}</style>
-    ${this.shape == Details.Shape.TRIANGLE
+  // Inner triangle
+  ...opacities.map(opacity => css`
+    :host([opacity="${opacity}"]) span {
+      opacity: ${1 - opacity / 2};
+    }`),
+
+  // Inner square and circle
+  ...colors.flatMap(color => opacities.map(opacity => css`
+    :host([shape="${Details.Shape.SQUARE}"][color="${color}"][opacity="${opacity}"]),
+    :host([shape="${Details.Shape.CIRCLE}"][color="${color}"][opacity="${opacity}"]) {
+        background-color: ${cssColor(color, opacity)};
+    }`))]
+
+  protected readonly render = () =>
+    this.shape == Details.Shape.TRIANGLE
       ? html`<span></span>`
-      : null}`
+      : null
 }
