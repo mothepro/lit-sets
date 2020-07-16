029cdaf4b2697b6003e2b84dac0740b9a5dbca78
mothepro <4e688d8adf9b8f68774900f9809778ccb7ef857a>
2020-07-16 02:26:01 -0500

    Updates

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
-import { LitElement, customElement, property, html, css, internalProperty, PropertyValues } from 'lit-element'
-import Game, { Player, Details, Card } from 'sets-game-engine'
-import type { peers, broadcast, random } from 'lit-p2p'
-import { TakeEvent, card } from '../index.js'
+import { LitElement, customElement, html, css, internalProperty, PropertyValues } from 'lit-element'
+import Game, { Player, Details, Card, CardSet } from 'sets-game-engine'
+import type { TakeEvent } from '../index.js'
 
 import 'lit-p2p'
 import 'lit-confetti'
@@ -16,29 +15,23 @@ import '../index.js'
 @customElement('p2p-sets')
 export default class extends LitElement {
 
-  @property({ attribute: false })
-  peers!: peers
-
-  @property({ attribute: false })
-  broadcast!: broadcast
-
-  @property({ attribute: false })
-  random!: random
-
   @internalProperty()
   protected confetti = 0
 
   @internalProperty()
   protected showClock = true
 
+  @internalProperty()
+  protected restartClock = false
+
   /** The sets game engine */
   private engine!: Game
 
   /** The instance my player in the game engine */
-  mainPlayer!: Player
+  private mainPlayer!: Player
 
   /** Scores of all the players every tick */
-  runningScores: number[][] = []
+  private runningScores: number[][] = []
 
   static readonly styles = [css`
     lit-confetti {
@@ -68,7 +61,7 @@ export default class extends LitElement {
     }`,
 
   // CSS vars for the colors in the chart
-  ...[...Array(10)].map((_, i) => css`
+  ...[...Array(10).keys()].map(i => css`
       ::part(for-${i}) {
         stroke: var(--chart-color-${i});
       }
@@ -76,65 +69,68 @@ export default class extends LitElement {
         background-color:  var(--chart-color-${i});
       }`)]
 
-  updated(changed: PropertyValues) {
-    if (changed.has('peers') && this.peers) {
-      this.peers.map(this.bindPeer)
-      delete this.engine // We need to remake this game
-    }
-
-    if (!this.engine && this.peers && this.random) {
-      // Shuffle cards
-      const cards = [...Array(Details.COMBINATIONS)].map((_, i) => Card.make(i))
-      for (let i = cards.length - 1; i > 0; i--) {
-        const j = Math.floor(this.random() * i)
-          ;[cards[j], cards[i]] = [cards[i], cards[j]]
-      }
-
-      this.engine = new Game([...Array(this.peers.length)].map(() => new Player), cards)
-
-      // Set main player
-      for (const [index, { isYou }] of this.peers.entries())
-        if (isYou)
-          this.mainPlayer = this.engine.players[index]
-
-      // Reset running scores
-      this.runningScores = this.engine.players.map(() => [])
-
-      // Refresh when market changes OR when the player performs some actions. */
-      this.engine.filled
-        .on(() => this.requestUpdate())
-        .then(() => {
-          this.confetti = 100
-          setTimeout(() => this.confetti = 0, 10 * 1000)
-        })
-      this.mainPlayer.hintUpdate.on(() => this.requestUpdate())
-      this.mainPlayer.unban.on(() => this.requestUpdate())
-      this.mainPlayer.ban.on(() => this.requestUpdate())
-    }
+  protected async firstUpdated() {
+    addEventListener('p2p-update', this.go)
+    this.go()
   }
 
-  /** Works on the engine on behalf of a peer */
-  private bindPeer = async ({ message, close }: peers[0], index: number) => {
+  updated(changed: PropertyValues) {
+    if (changed.has('restartClock') && this.restartClock)
+      this.restartClock = false
+  }
+
+  private go = async () => {
+    // Shuffle all cards using given RNG
+    const cards: Card[] = [...Array(Details.COMBINATIONS)].map((_, i) => Card.make(i))
+    for (let i = cards.length - 1; i > 0; i--) {
+      const j = Math.abs(p2p.random(true)) % i
+        ;[cards[j], cards[i]] = [cards[i], cards[j]]
+    }
+
+    this.engine = new Game([...Array(p2p.peers.length)].map(() => new Player), cards)
+    p2p.peers.map(this.bindPeer)
+
+    // Reset running scores
+    this.runningScores = this.engine.players.map(() => [])
+    this.restartClock = true
+
+    // Refresh when market changes OR when the player performs some actions. */
+    this.mainPlayer.hintUpdate.on(() => this.requestUpdate())
+    this.mainPlayer.unban.on(() => this.requestUpdate())
+    this.mainPlayer.ban.on(() => this.requestUpdate())
+    for await (const _ of this.engine.filled)
+      this.requestUpdate()
+    this.confetti = 100
+    setTimeout(() => this.confetti = 0, 10 * 1000)
+  }
+
+  /** Works on the engine on behalf of a peer & sets main player */
+  private bindPeer = async ({ message, close, isYou }: typeof p2p.peers[0], index: number) => {
+    if (isYou)
+      this.mainPlayer = this.engine.players[index]
+
     try {
       for await (const data of message)
-        switch ((data as ArrayBuffer).byteLength ?? 0) {
-          case 1: // Hint
-            this.engine.takeHint(this.engine.players[index])
-            break
+        if (data instanceof ArrayBuffer)
+          switch (data.byteLength) {
+            case 1: // Hint
+              this.engine.takeHint(this.engine.players[index])
+              break
 
-          case 3: // Take
-            //TODO: pack this to 1 (or 2) bytes, using `detail` as a boolean list
-            this.engine.takeFromMarket(
-              this.engine.players[index],
-              [...new Uint8Array(data as ArrayBuffer)] as [number, number, number])
-            break
+            case 3: // Take
+              const indexs = new Set(new Uint8Array(data))
+              //TODO: pack this to 1 (or 2) bytes, using `detail` as a boolean list
+              this.engine.takeSet(
+                this.engine.players[index],
+                this.engine.cards.filter((_, i) => indexs.has(i)) as CardSet)
+              break
 
-          default:
-            throw Error(`Unexpected data from ${name}: ${data}`)
-        }
+            default:
+              throw Error(`Unexpected data from ${name}: ${data}`)
+          }
     } catch (error) {
-      this.dispatchEvent(new ErrorEvent('p2p-error', { error }))
-      console.error('Lost connection with', name, error)
+      error.peer = name
+      this.dispatchEvent(new ErrorEvent('p2p-error', { error, bubbles: true, composed: true }))
     }
     close()
   }
@@ -143,16 +139,15 @@ export default class extends LitElement {
     const winners: string[] = []
     for (const [index, { score }] of this.engine.players.entries())
       if (score == this.engine.maxScore)
-        winners.push(this.peers[index].isYou
+        winners.push(p2p.peers[index].isYou
           ? 'You'
-          : this.peers[index].name)
+          : p2p.peers[index].name)
     return `${winners.join(' & ')} Win${winners.length == 1 && winners[0] != 'You' ? 's' : ''}`
   }
 
-  protected readonly render = () => this.engine && this.mainPlayer && (
-    this.engine.filled.isAlive
-      // In game
-      ? html`
+  protected readonly render = () => p2p && this.engine && this.mainPlayer && (this.engine.filled.isAlive
+    // In game
+    ? html`
       <lit-sets
         part="sets"
         ?hint-available=${this.mainPlayer.hintCards.length < 3}
@@ -161,19 +156,21 @@ export default class extends LitElement {
         take-on-key="Enter"
         .cards=${this.engine.cards}
         .hint=${this.mainPlayer.hintCards}
-        @take=${({ detail }: TakeEvent) => this.broadcast(new Uint8Array(detail))}
-        @hint=${() => this.broadcast(new Uint8Array([0]))}
+        @take=${({ detail }: TakeEvent) => p2p.broadcast(new Uint8Array(detail))}
+        @hint=${() => p2p.broadcast(new Uint8Array([0]))}
       ></lit-sets>
       <sets-leaderboard
         part="leaderboard ${`leaderboard-${this.engine.players.length == 1 ? 'simple' : 'full'}`}"
         .players=${this.engine.players}
-        .names=${this.peers?.map(peer => peer.name) ?? []}
+        .names=${p2p.peers?.map(peer => peer.name) ?? []}
       ></sets-leaderboard>
       <lit-clock
         part="clock"
+        .ticks=${this.restartClock ? 0 : null}
         ?hidden=${!this.showClock}
         ?pause-on-blur=${this.engine.players.length == 1}
-        @tick=${() => this.engine.players.map(({ score }, index) => this.runningScores[index].push(score))}
+        @tick=${() => this.engine.players.map(({ score }, index) => this.engine.filled.isAlive
+          && this.runningScores[index].push(score))}
       ></lit-clock>
       <mwc-fab
         part="clock-toggle"
@@ -185,20 +182,20 @@ export default class extends LitElement {
         title=${this.showClock ? 'Hide time' : 'Show time'}
       ></mwc-fab>`
 
-      // Game over
-      : html`
+    // Game over
+    : html`
       <lit-confetti gravity=1 count=${this.confetti}></lit-confetti>
       ${this.engine.players.length > 1 ? html`<h2 part="title">${this.winnerText}!</h2>` : ''}
       Finished ${this.runningScores[0].length} seconds!<br/>
       <lit-chart
         part="chart"
-        width="500"
-        height="300"
+        width=${document.body.clientWidth}
+        height=${Math.floor(document.body.clientWidth * 4 / 9)}
         .data=${this.runningScores}
       ></lit-chart>
       ${this.engine.players.length > 1 // TODO legend should live inside the chart
-          ? html`
-        <legend part="legend">${this.peers.map(({ name }, index) => html`
+        ? html`
+        <legend part="legend">${p2p.peers.map(({ name }, index) => html`
           <div part="legend-for legend-for-${index}" class="for for-${index}">
             <div class="block"></div>
             ${name}
