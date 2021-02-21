8d5499fe0326f9da6878199343a05576b098eebc
Maurice Prosper <mothepro@live.com>
2021-02-21 13:43:03 -0600

    Refactor index js

--- demo/index.ts
+++ demo/index.ts
@@ -2,11 +2,30 @@ import 'lit-p2p'                // <lit-p2p>
 import './p2p-sets.js'          // <p2p-sets>
 import '@mothepro/theme-toggle' // <theme-toggle>
 import '@material/mwc-dialog'   // <mwc-dialog>
+declare global {
+  interface Window {
+    dataLayer: unknown[]
+  }
+}
+
+function gtag(...args: any[]) {
+  window.dataLayer.push(arguments)
+}
+
+
+// Initialize deferredPrompt for use later to show browser install prompt.
+let deferredPrompt: Event | void
+window.dataLayer = window.dataLayer || []
+
+// Google Analytics
+gtag('js', new Date)
+gtag('config', 'UA-172429940-2')
 
 const
   toggleOnlineBtns = document.querySelectorAll('[toggle-online]')!,
   litP2pElement = document.querySelector('lit-p2p')!,
   helpBtn = document.querySelector('mwc-icon-button[icon=help]')!,
+  // installBtn = document.querySelector('mwc-icon-button[icon=download]')!,
   dialogElement = document.querySelector('mwc-dialog')!
 
 // Make the toggle button actually do something
@@ -18,7 +37,7 @@ for (const toggleOnlineBtn of toggleOnlineBtns)
       : '-1')) // not trying to connect
 
 // Add [open] to <mwc-dialog> after some time if first visit
-if (!localStorage.length)
+if (!localStorage.length && document.body.hasAttribute('first-visit-help-delay'))
   setTimeout(
     () => dialogElement.setAttribute('open', ''),
     parseInt(document.body.getAttribute('first-visit-help-delay') ?? ''))
@@ -26,3 +45,38 @@ if (!localStorage.length)
 // Show help
 helpBtn.addEventListener('click', () => dialogElement.toggleAttribute('open'))
 addEventListener('keypress', ({ key }: KeyboardEvent) => key == '?' && dialogElement.toggleAttribute('open'))
+
+// addEventListener('beforeinstallprompt', event => {
+//   event.preventDefault()
+//   deferredPrompt = event
+//   installBtn.removeAttribute('hidden')
+//   ga('send', 'install', 'prompt')
+// })
+
+// installBtn.addEventListener('click', async () => {
+//   installBtn.toggleAttribute('hidden')
+//   if (deferredPrompt)
+//     deferredPrompt.prompt()
+//   // Wait for the user to respond to the prompt
+//   const { outcome } = await deferredPrompt.userChoice;
+//   // Optionally, send analytics event with outcome of user choice
+//   console.log(`User response to the install prompt: ${outcome}`)
+//   deferredPrompt = undefined;
+// });
+
+// addEventListener('appinstalled', () => {
+//   // Clear the deferredPrompt so it can be garbage collected
+//   deferredPrompt = undefined
+//   // Optionally, send analytics event to indicate successful install
+//   console.log('PWA was installed')
+// })
+
+// Error logging
+// @ts-ignore Event listerner types are garbage
+document.body.firstElementChild!.addEventListener('p2p-error', (error: ErrorEvent) => {
+  console.error('Lost connection', error)
+  ga('send', 'exception', {
+    exDescription: `${error.message} -- ${(error as unknown as Error).stack}`,
+    exFatal: false, // idk...
+  })
+})
